8adc8dffba198edd3f78cd21f0dea93561661691
Bruno Marée <bruno.maree@student.hepl.be>
2024-11-14 19:52:13 +0100

    :+1: Created nav and button components

---
+++ resources/views/components/button/button-confirm.blade.php
@@ -0,0 +1,1 @@
+<button type="submit" class="button-confirm p-3">hey</button>
---
+++ resources/views/components/navigations/nav-item.blade.php
@@ -0,0 +1,10 @@
+<li class="rounded-xl nav_item_hover p-2.5">
+    <a class="flex-row flex-auto  justify-start items-center gap-2 flex" href="">
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none" class="relative">
+            <path
+                d="M8.25 20.1667V11H13.75V20.1667M2.75 8.24999L11 1.83333L19.25 8.24999V18.3333C19.25 18.8196 19.0568 19.2859 18.713 19.6297C18.3692 19.9735 17.9029 20.1667 17.4167 20.1667H4.58333C4.0971 20.1667 3.63079 19.9735 3.28697 19.6297C2.94315 19.2859 2.75 18.8196 2.75 18.3333V8.24999Z"
+                stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <p class="text-black leading-snug">Home</p>
+    </a>
+</li>
