b5a4937d68481c883acfdb7ad0ed39f829ae35c0
Bruno Marée <bruno.maree@student.hepl.be>
2024-12-26 15:36:01 +0100

    ✅ Expense done

--- app/Livewire/BankExpenses.php
+++ app/Livewire/BankExpenses.php
@@ -2,10 +2,41 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Form\TransactionsForm;
+use App\Models\Fund as FundModel;
+use App\Models\Transaction;
 use Livewire\Component;
 
 class BankExpenses extends Component
 {
+    public $funds;
+    public $transaction;
+    public TransactionsForm $form;
+    public $feedback = '';
+
+    public function mount(Transaction $transaction)
+    {
+        $this->funds = FundModel::get();
+        $this->form->setTransactions($transaction);
+    }
+
+    public function retrievingFunds()
+    {
+        return Transaction::whereIn('fund_id', $this->funds->pluck('id'))->get();
+    }
+    public function save()
+    {
+        // Force le montant de la dépense à être négatif
+        $this->form->amount = -abs($this->form->amount);
+
+        // Puis on stocke
+        $this->form->store();
+
+        $this->feedback = 'Dépense enregistrée !';
+        return $this->redirect('/funds');
+    }
+
+
     public function render()
     {
         return view('livewire.bank-expenses');
