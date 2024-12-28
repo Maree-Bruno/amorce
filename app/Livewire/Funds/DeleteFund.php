4a9a19db90e34023e5d0ff050dcd08ae4992ebe4
Bruno Marée <bruno.maree@student.hepl.be>
2024-12-28 16:28:01 +0100

    ✅ Done

--- app/Livewire/Funds/DeleteFund.php
+++ app/Livewire/Funds/DeleteFund.php
@@ -2,10 +2,33 @@
 
 namespace App\Livewire\Funds;
 
+use App\Models\Fund;
 use Livewire\Component;
 
 class DeleteFund extends Component
 {
+    public $fundId;
+    public $funds = [];
+
+    public function mount()
+    {
+        $this->funds= Fund::all();
+    }
+
+    public function deleteFund()
+    {
+        $fund = Fund::find($this->fundId);
+
+        if ($fund) {
+            $fund->delete(); // Soft delete
+            session()->flash('message', 'Le fond a été supprimé avec succès.');
+        } else {
+            session()->flash('error', 'Le fond est introuvable.');
+        }
+
+        return $this->redirect('/funds');
+    }
+
     public function render()
     {
         return view('livewire.funds.delete-fund');
--- resources/views/livewire/funds/delete-fund.blade.php
+++ resources/views/livewire/funds/delete-fund.blade.php
@@ -1,3 +1,26 @@
 <x-modals.modal>
+    <div>
+        <form wire:submit.prevent="deleteFund">
+            <select wire:model="fundId" name="fund" id="fund" class="form-select">
+                <option value="">Sélectionnez un fond</option>
+                @foreach($funds as $fund)
+                    <option value="{{ $fund->id }}" wire:key="{{$fund->id }}">{{ $fund->name }}</option>
+                @endforeach
+            </select>
 
+            <!-- Bouton de suppression -->
+            <x-button.button type="submit" class="buttons-confirm">
+                Supprimer le fond
+            </x-button.button>
+        </form>
+
+        <!-- Messages de feedback -->
+        @if(session()->has('message'))
+            <div class="text-green-500">{{ session('message') }}</div>
+        @endif
+
+        @if(session()->has('error'))
+            <div class="text-red-500">{{ session('error') }}</div>
+        @endif
+    </div>
 </x-modals.modal>
