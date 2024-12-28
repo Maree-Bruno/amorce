4b8a535eb2fbf5c0e3d413c9e63ad410564c9af1
Bruno Marée <bruno.maree@student.hepl.be>
2024-12-28 21:27:55 +0100

    ✅ done

--- routes/transaction.php
+++ routes/transaction.php
@@ -5,6 +5,7 @@ use App\Livewire\BankAddMultipleDonation;
 use App\Livewire\BankCreateDonation;
 use App\Livewire\BankExpenses;
 use App\Livewire\BankTransfert;
+use App\Livewire\Transactions\Edit;
 use App\Livewire\Transactions\Transaction;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +14,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/transactions/create/donations', BankAddMultipleDonation::class)->name('transactions.create.donations');
     Route::get('/transactions/create/transfer', BankTransfert::class)->name('transactions.create.transfer');
     Route::get('/transactions/create/spent', BankExpenses::class)->name('transactions.create.spent');
+    Route::get('/transactions/{transaction}/edit', Edit::class)->name('transactions.edit');
 
 });
