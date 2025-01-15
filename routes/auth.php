824ebd119055db8bdaee2f7052f73c58249f4319
Bruno Marée <bruno.maree@student.hepl.be>
2025-01-15 18:57:22 +0100

    trying thing with deploy

--- routes/auth.php
+++ routes/auth.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
+    Route::get('/', [RegisteredUserController::class, 'create'])
+        ->name('login');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
