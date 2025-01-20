3e03febe0af5c46d9bc3c300cc612434afa7abe7
Bruno Marée <bruno.maree@student.hepl.be>
2025-01-20 11:39:44 +0100

    update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -12,15 +12,14 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    /*Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
-    Route::get('/', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register', [RegisteredUserController::class, 'store']);*/
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
-
+    Route::get('/', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
