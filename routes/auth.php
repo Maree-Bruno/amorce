13d89b558158725cfc1a422d715e57fa2a9e049e
Bruno Marée <bruno.maree@student.hepl.be>
2025-08-26 20:52:22 +0200

    update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -12,13 +12,8 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', function () {
-        return redirect()->route('login');
-    });
+   
     Route::get('/', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
